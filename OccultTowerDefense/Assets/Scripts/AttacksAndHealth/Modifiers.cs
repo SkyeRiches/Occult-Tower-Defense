faec741d0638c4e534042197ac41dfbf3c31a7c2
Jack Kellett <jackkellett02@gmail.com>
2022-08-22 21:38:27 +0100

    Modifier update.
    Made application of modifiers possible now.
    
    Health modifiers now work and can be applied to anything with a health script.
    
    API functionality to remove the modifiers has been scripted but a way to call the API needs to be created.
    
    Other modifiers to be announced.
    
    A purely healthMultiplier attack has been created.

--- OccultTowerDefense/Assets/Scripts/AttacksAndHealth/Modifiers.cs
+++ OccultTowerDefense/Assets/Scripts/AttacksAndHealth/Modifiers.cs
@@ -9,11 +9,15 @@ namespace Modifiers {
 		healthMultiplier = 1,
 		attackDamageMultiplier = 2,
 	}
+
+	[System.Serializable]
 	public class Modifier {
 		//Public Variables.
-		public int ID = 0;
+		public readonly int ID = 0;
 		public ModifierType type = ModifierType.none;
 		public float value = 0.0f;
+		public float modifierTime = 1.0f;
+		public bool useTimer = true;
 
 		//Private Variables.
 		private static int modifierCount = 0;
@@ -21,12 +25,24 @@ namespace Modifiers {
 		//Private Functions.
 
 		//Public Functions.
-		public Modifier(ModifierType a_type, float a_value)
+		public Modifier(ModifierType a_type, float a_value, float a_modifierTime, bool a_useModifierTime)
 		{
 			ID = modifierCount;
 			modifierCount++;
 			type = a_type;
 			value = a_value;
+			modifierTime = a_modifierTime;
+			useTimer = a_useModifierTime;
+		}
+
+		public Modifier(Modifier a_copy)
+		{
+			ID = modifierCount;
+			modifierCount++;
+			type = a_copy.type;
+			value = a_copy.value;
+			modifierTime = a_copy.modifierTime;
+			useTimer = a_copy.useTimer;
 		}
 	}
 }
