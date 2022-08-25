e2dd0c40a3a927de5d706683a3c91b0424195382
Jack Kellett <jackkellett02@gmail.com>
2022-08-25 21:10:01 +0100

    Got a basic wave spawner working.
    Got a basic wave spawner working.
    
    Whenever a wave is started each enemy is placed at a spawn point.
    
    To start the next wave there is a function called StartNextWave() that can be put on a button I guess to start the next wave available. If no waves are available then it will set a boolean called "gameOver" to true.
    
    That gameOver boolean can be accessed and checked with another public bool function called "IsGameOver()".

--- OccultTowerDefense/Assets/Scripts/WaveStuff/WaveManagerScript.cs
+++ OccultTowerDefense/Assets/Scripts/WaveStuff/WaveManagerScript.cs
@@ -9,28 +9,97 @@ public class WaveManagerScript : MonoBehaviour {
 	private Transform spawnPoint = null;
 
 	[SerializeField]
+	private float spawnRateLowerRange = 0.1f;
+
+	[SerializeField]
+	private float spawnRateUpperRange = 1.0f;
+
+	[SerializeField]
 	private List<Wave> gameWaves = null;
 	#endregion
 
 	#region Private Variables.
 
+	private bool waveStarted = false;
+
+	private Wave currentWave = null;
 	private bool waveOver = false;
+	private bool spawnCooldown = false;
+
+	private bool gameOver = false;
 	#endregion
 
 	#region Private Functions.
 	// Start is called before the first frame update
 	void Start() {
-
+		//StartNextWave();
 	}
 
 	// Update is called once per frame
 	void Update() {
+		if (waveStarted && currentWave != null) {
+			if (!spawnCooldown) {
+				SpawnEnemy(currentWave.GetNextEnemey());
+				spawnCooldown = true;
+				StartCoroutine(SpawnCooldown());
+			}
 
+			if (currentWave.IsWaveOver()) {
+				//Finish the wave.
+				currentWave = null;
+				waveStarted = false;
+			}
+		}
+	}
+
+	private void SpawnEnemy(WaveInfo a_enemy) {
+		if (a_enemy.enemyCount <= 0) {
+			return;
+		}
+
+		if (a_enemy.enemyPrefab == null) {
+			return;
+		}
+
+		GameObject enemyToSpawn = Instantiate(a_enemy.enemyPrefab, spawnPoint.position, spawnPoint.rotation, spawnPoint);
+
+	}
+
+	private IEnumerator SpawnCooldown() {
+		float cooldownTime = Random.Range(spawnRateLowerRange, spawnRateUpperRange);
+		yield return new WaitForSeconds(cooldownTime);
+		spawnCooldown = false;
 	}
 	#endregion
 
 	#region Public Access Functions (Getters and Setters).
 
+	public void StartNextWave() {
+		if (gameWaves == null) {
+			Debug.LogError("No valid waves available.");
+			return;
+		}
+
+		if (gameWaves.Count <= 0) {
+			gameOver = true;
+			return;
+		}
+
+		if (!waveStarted) {
+			currentWave = gameWaves[0];
+			gameWaves.Remove(currentWave);
+			waveStarted = true;
+		}
+	}
+
+	/// <summary>
+	/// Returns true if there are no waves left.
+	/// </summary>
+	/// <returns></returns>
+	public bool IsGameOver()
+	{
+		return gameOver;
+	}
 	#endregion
 
 	#region Validation
