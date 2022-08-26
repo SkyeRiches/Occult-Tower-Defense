e8382bd43e4303d19015d99e5c3a4ee3d305587a
JoshuaRiches <71353999+JoshuaRiches@users.noreply.github.com>
2022-08-26 20:00:45 +0100

    game manager

--- OccultTowerDefense/Assets/Scripts/GameManager.cs
+++ OccultTowerDefense/Assets/Scripts/GameManager.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject winMessage;
+    [SerializeField] private GameObject failMessage;
+
     public void FailGame()
     {
-
+        winMessage.SetActive(true);
+        StartCoroutine(LoadMenu());
     }
 
     public void WinGame()
     {
+        failMessage.SetActive(true);
+        StartCoroutine(LoadMenu());
+    }
 
+    private IEnumerator LoadMenu()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(0);
     }
 }
